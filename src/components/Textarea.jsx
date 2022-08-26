d8a4773f8ae8ec75b9f754ae8860cbe4eb8ffd36
union-NandM <not.tomoharu@gmail.com>
2022-08-26 16:43:37 +0900

    textarea内の文字をmonospaceに

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -5,7 +5,7 @@ export const Textarea = ({ text, setText }) => {
   return (
     <div>
       <textarea
-        className="border-solid border-gray-300 border-2 w-full block h-32 resize-none"
+        className="border-solid border-gray-300 border-2 w-full block h-32 resize-none font-mono"
         onChange={handleChange}
         value={text}
       ></textarea>
